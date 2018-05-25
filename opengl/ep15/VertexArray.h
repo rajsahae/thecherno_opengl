b3cf5ea6149358b86a7708aea6eeb064d03989a7
Raj Sahae <rsahae@tesla.com>
2018-05-24 23:22:08 -0700

    initial commit for ep15, plus vim session

---
+++ opengl/ep15/VertexArray.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "VertexBuffer.h"
+#include "VertexBufferLayout.h"
+
+class VertexArray
+{
+    private:
+        unsigned int m_RendererID;
+
+    public:
+        VertexArray();
+        ~VertexArray();
+
+        void AddBuffer(const VertexBuffer& vb, const VertexBufferLayout& layout);
+        void Bind() const;
+        void Unbind() const;
+};
---
+++ opengl/ep15/VertexBuffer.h
@@ -0,0 +1,14 @@
+#pragma once
+
+class VertexBuffer
+{
+    public:
+        VertexBuffer(const void* data, unsigned int size);
+        ~VertexBuffer();
+
+        void Bind() const;
+        void Unbind() const;
+
+    private:
+        unsigned int m_RendererID;
+};
