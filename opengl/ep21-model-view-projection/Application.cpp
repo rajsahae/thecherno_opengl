1f3f85313d58a0aaf461e8d379fbe22b673f79e9
Raj Sahae <rsahae@tesla.com>
2018-05-28 10:11:50 -0700

    playing with mvp matrices

--- opengl/ep21-model-view-projection/Application.cpp
+++ opengl/ep21-model-view-projection/Application.cpp
@@ -68,8 +68,8 @@ int main( void )
 
     float positions[] = {
         100.0f, 100.0f, 0.0f, 0.0f, // 0
-         200.0f, 100.0f, 1.0f, 0.0f, // 1
-         200.0f, 200.0f, 1.0f, 1.0f, // 2
+        200.0f, 100.0f, 1.0f, 0.0f, // 1
+        200.0f, 200.0f, 1.0f, 1.0f, // 2
         100.0f, 200.0f, 0.0f, 1.0f  // 3
     };
 
@@ -87,9 +87,18 @@ int main( void )
         IndexBuffer ib(indices, 6);
 
         glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-        glm::vec4 vp(100.0f, 100.0f, 0.0f, 1.0f);
 
-        glm::vec4 result = proj * vp;
+        glm::mat4 ident = glm::mat4(1.0f);
+        glm::vec3 trvec = glm::vec3(-100, 0, 0);
+        glm::mat4 view = glm::translate(ident, trvec);
+
+        glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
+
+        glm::mat4 mvp = proj * view * model;
+
+        // projection matrix
+        // view matrix
+        // model matrix
 
         VertexBufferLayout layout;
         layout.AddFloat(2);
@@ -100,7 +109,7 @@ int main( void )
         Shader shader("res/shaders/Basic.shader");
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.0f, 0.3f, 0.8f, 1.0f);
-        shader.SetUniformMat4f("u_MVP", proj);
+        shader.SetUniformMat4f("u_MVP", mvp);
 
         Texture texture("res/textures/phone.png");
         texture.Bind();
