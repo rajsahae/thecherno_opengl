755ea67b09b6027df9e6d146714b72b202343f56
Raj Sahae <rsahae@tesla.com>
2018-05-29 01:13:23 -0700

    Use imgui window sliders for object translation

--- opengl/ep22-imgui/Application.cpp
+++ opengl/ep22-imgui/Application.cpp
@@ -89,19 +89,13 @@ int main( void )
         VertexBuffer vb(positions, 4 * 4 * sizeof(float));
         IndexBuffer ib(indices, 6);
 
+        // projection matrix
         glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
 
+        // view matrix
         glm::mat4 ident = glm::mat4(1.0f);
         glm::vec3 trvec = glm::vec3(-100, 0, 0);
         glm::mat4 view = glm::translate(ident, trvec);
-
-        glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
-
-        glm::mat4 mvp = proj * view * model;
-
-        // projection matrix
-        // view matrix
-        // model matrix
 
         VertexBufferLayout layout;
         layout.AddFloat(2);
@@ -112,7 +106,6 @@ int main( void )
         Shader shader("res/shaders/Basic.shader");
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.0f, 0.3f, 0.8f, 1.0f);
-        shader.SetUniformMat4f("u_MVP", mvp);
 
         Texture texture("res/textures/phone.png");
         texture.Bind();
@@ -128,35 +121,25 @@ int main( void )
         ImGui_ImplGlfwGL3_Init(window, true);
         ImGui::StyleColorsDark();
 
-        bool show_demo_window = true;
-        bool show_another_window = false;
-        ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+        glm::vec3 translation(200, 200, 0);
 
         do {
             renderer.Clear();
 
             ImGui_ImplGlfwGL3_NewFrame();
 
+            // model matrix
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
+            glm::mat4 mvp = proj * view * model;
+
             shader.Bind();
             shader.SetUniform4f("u_Color", red, 0.3, 0.8, 1.0);
+            shader.SetUniformMat4f("u_MVP", mvp);
 
             renderer.Draw(va, ib, shader);
 
             {
-                static float f = 0.0f;
-                static int counter = 0;
-                ImGui::Text("Hello, world!");                           // Display some text (you can use a format string too)
-                ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
-                ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-                ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our windows open/close state
-                ImGui::Checkbox("Another Window", &show_another_window);
-
-                if (ImGui::Button("Button"))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
-                    counter++;
-                ImGui::SameLine();
-                ImGui::Text("counter = %d", counter);
-
+                ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);
                 ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
             }
 
